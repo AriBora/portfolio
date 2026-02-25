3cade387eef1366119593839e726fb1950f9a5c1
AriBora <arindom21@iitk.ac.in>
2026-02-25 20:23:36 +0530

    Removed browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme/Theme';
+import PortfolioLayout from './components/PortfolioLayout';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <PortfolioLayout />
+  </ThemeProvider>
 );
 
 export default App;
